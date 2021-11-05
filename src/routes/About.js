184a4e0f93e74490badc1f1e191bfddf1daddd66
sungjoo224 <sdd8448@naver.com>
2021-11-05 12:28:21 +0900

    first commit

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div>
+            <div className="about_container">
+                <span>"A JavaScript library for building user interfaces"</span>
+                <spn>- React -</spn>
+            </div>
+        </div>
+    )
+}
+
+export default About;
